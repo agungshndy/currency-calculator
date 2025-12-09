cc21a94bdb0e9362bead84e0a9bd0ad0d595872c
agungshndy <agungshandy43@gmail.com>
2025-12-09 16:30:38 +0700

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Start calculating your favourite currency here with our online currency calculator!
+      Get the accurate exchange rates from many currencies.
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.1rem;
+  font-weight: 300;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
